f38ae6d9d94bd3dda9fb5fa3d308bc38e7a4dc68
廖展亮 <1019304274@qq.com>
2019-03-10 11:47:15 +0800

    update 更新swagger ui和相关配置

--- src/main/java/com/pjb/controller/UserInfoController.java
+++ src/main/java/com/pjb/controller/UserInfoController.java
@@ -1,16 +1,20 @@
 package com.pjb.controller;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
+
 import com.pjb.entity.UserInfo;
 import com.pjb.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-
+@Api(description = "用户接口")
 @Controller
 @RequestMapping("/alpha")
 public class UserInfoController {
@@ -19,32 +23,38 @@ public class UserInfoController {
 
     /**
      * 用户查询.
+     *
      * @return
      */
-    @RequestMapping("/userList")
+    @ApiOperation(value = "用户查询", notes = "", produces = "application/json")
+    @GetMapping("/userList")
     @ResponseBody
 //    @RequiresPermissions("userInfo:view")//权限管理;
-   public List<UserInfo> getUser( ){
+    public List<UserInfo> getUser() {
         return userService.getUserInfo();
     }
 
     /**
-     * 用户添加;
+     * 用户删除;
+     *
      * @return
      */
-    @RequestMapping("/userAdd")
-//    @RequiresPermissions("userInfo:add")//权限管理;
-    public String userInfoAdd(){
-        return "userInfoAdd";
+    @ApiOperation(value = "用户删除", notes = "", produces = "application/json")
+    @GetMapping("/userDel")
+//    @RequiresPermissions("userInfo:del")//权限管理;
+    public String userDel() {
+        return "userInfoDel";
     }
 
     /**
-     * 用户删除;
+     * 用户添加;
+     *
      * @return
      */
-    @RequestMapping("/userDel")
-//    @RequiresPermissions("userInfo:del")//权限管理;
-    public String userDel(){
-        return "userInfoDel";
+    @ApiOperation(value = "用户添加", notes = "", produces = "application/json")
+    @GetMapping("/userAdd")
+//    @RequiresPermissions("userInfo:add")//权限管理;
+    public String userInfoAdd() {
+        return "userInfoAdd";
     }
 }
