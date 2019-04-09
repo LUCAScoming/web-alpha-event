3e682d20721d22345d2cc239d713239224c644ee
廖展亮 <1019304274@qq.com>
2019-04-09 21:06:31 +0800

    test

--- src/main/java/com/pjb/entity/SysPermission.java
+++ src/main/java/com/pjb/entity/SysPermission.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class SysPermission {
     Integer id;//主键.
     String name;//名称.
